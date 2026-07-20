faf5d5ed9620d79d21b15b6a7c0e1e31561ed7e4
rebecca3miller <rebecca.miller@keyin.com>
2026-07-20 14:00:24 -0230

    collegestudent.java file extends to student. Constructor and methods completed. Demo.java set up, added data for testing.

--- PROBLEM-1/CollegeStudent.java
+++ PROBLEM-1/CollegeStudent.java
@@ -1,5 +1,39 @@
-package PROBLEM-1;
-
-public class CollegeStudent {
+public class CollegeStudent extends Student {
     
+    private int year;
+    private String major;
+
+    public CollegeStudent(String name, int age, String gender, String idNum, double gpa, int year, String major) 
+    {
+        super(name, age, gender, idNum, gpa);
+
+        this.year = year;
+        this.major = major;
+    }
+
+    public int getYear() 
+    {
+        return this.year;
+    }
+
+    public void setYear(int year) 
+    {
+        this.year = year;
+    }
+
+    public String getMajor() 
+    {
+        return this.major;
+    }
+
+    public void setMajor(String major) 
+    {
+        this.major = major;
+    }
+
+    public String toString() 
+    {
+        return super.toString() + ", Year: " + year + ", Major: " + major;
+    }
+
 }
--- PROBLEM-1/Demo.java
+++ PROBLEM-1/Demo.java
@@ -1,5 +1,13 @@
-package PROBLEM-1;
-
 public class Demo {
-    
+    public static void main(String[] args) 
+    {
+        Person john = new Person("Coach John", 45, "Male");
+        System.out.println(john);
+
+        Student jane = new Student("Jane Doe", 20, "Female", "HS12345", 3.8);
+        System.out.println(jane);
+
+        CollegeStudent mike = new CollegeStudent("Mike Smith", 21, "Male", "CS67890", 3.5, 2, "Computer Science");
+        System.out.println(mike);
+    }        
 }

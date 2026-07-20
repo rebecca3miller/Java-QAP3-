df0ff8ab0fcf4b3f999aba8c183d075c8df4f3ad
rebecca3miller <rebecca.miller@keyin.com>
2026-07-20 15:39:08 -0230

    completed problem 3. copied path to work on problem 4.

---
+++ PROBLEM-4/EquilateralTriangle.java
@@ -0,0 +1,9 @@
+public class EquilateralTriangle extends Triangle {
+
+    public EquilateralTriangle(double side)
+    {
+        super(side, side, side);
+
+        name = "Equilateral Triangle";
+    }
+}

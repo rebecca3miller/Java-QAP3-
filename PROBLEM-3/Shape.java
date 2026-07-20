1e4b6c90608f65dbece8ce7983ed11be5efb67da
rebecca3miller <rebecca.miller@keyin.com>
2026-07-20 14:54:40 -0230

    Set up Shape.java

--- PROBLEM-3/Shape.java
+++ PROBLEM-3/Shape.java
@@ -1,5 +1,19 @@
-package PROBLEM-3;
-
 public class Shape {
-    
+
+    protected String name;
+
+    public Shape(String name) 
+    {
+        this.name = name;
+    }
+
+    public abstract double area();
+
+    public abstract double perimeter();
+
+    @Override
+    public String toString() 
+    {
+        return "Shape(" + name + "Area: " + area() + ", Perimeter: " + perimeter() + ")";
+    }
 }

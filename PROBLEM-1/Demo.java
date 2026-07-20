1fe9696c4e08ad5f21094df0d4c3a8c381a89165
rebecca3miller <rebecca.miller@keyin.com>
2026-07-20 14:03:45 -0230

    Forgot to add teacher data.

--- PROBLEM-1/Demo.java
+++ PROBLEM-1/Demo.java
@@ -7,6 +7,9 @@ public class Demo {
         Student jane = new Student("Jane Doe", 20, "Female", "HS12345", 3.8);
         System.out.println(jane);
 
+        Teacher mrBrown = new Teacher("Mr. Brown", 40, "Male", "Math", 50000);
+        System.out.println(mrBrown);
+
         CollegeStudent mike = new CollegeStudent("Mike Smith", 21, "Male", "CS67890", 3.5, 2, "Computer Science");
         System.out.println(mike);
     }        

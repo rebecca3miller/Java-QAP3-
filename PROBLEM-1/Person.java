0d5fe36a48e3e350172bfbbecd9913f942e0828e
rebecca3miller <rebecca.miller@keyin.com>
2026-07-20 13:35:40 -0230

    Created and set up person.java class

--- PROBLEM-1/Person.java
+++ PROBLEM-1/Person.java
@@ -1,5 +1,39 @@
-package PROBLEM-1;
-
 public class Person {
     
+    protected String myName;
+    protected int myAge;
+    protected String myGender;
+
+    public Person(String name, int age, String gender) 
+    {
+        myName = name;
+        myAge = age;
+        myGender = gender;
+    }
+
+    public String getName() 
+    {
+        return this.myName;
+    }
+
+    public int getAge() 
+    {
+        return this.myAge;
+    }
+
+    public String getGender() 
+    {
+        return this.myGender;
+    }
+
+    public void setGender(String gender) 
+    {
+        this.myGender = gender;
+    }
+
+    public String toString() 
+    {
+        return myName + ", Age: " + myAge + ", Gender: " + myGender;
+    }
+
 }

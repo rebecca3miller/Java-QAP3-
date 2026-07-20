a1f9fcc35fb911bcaa802dc64bc910e10ffc2c3d
rebecca3miller <rebecca.miller@keyin.com>
2026-07-20 13:47:25 -0230

    Teacher extends person. Constructor and methods created.

--- PROBLEM-1/Teacher.java
+++ PROBLEM-1/Teacher.java
@@ -1,5 +1,39 @@
-package PROBLEM-1;
-
-public class Teacher {
+public class Teacher extends Person {
     
+    private String subject;
+    private double salary;
+
+    public Teacher(String name, int age, String gender, String subject, double salary) 
+    {
+        super(name, age, gender);
+
+        this.subject = subject;
+        this.salary = salary;
+    }
+
+    public String getSubject() 
+    {
+        return this.subject;
+    }
+
+    public void setSubject(String subject) 
+    {
+        this.subject = subject;
+    }
+
+    public double getSalary() 
+    {
+        return this.salary;
+    }
+
+    public void setSalary(double salary) 
+    {
+        this.salary = salary;
+    }
+
+    public String toString() 
+    {
+        return super.toString() + ", Subject: " + subject + ", Salary: " + salary;
+    }
+
 }

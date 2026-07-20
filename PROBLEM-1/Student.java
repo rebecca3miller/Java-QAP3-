de59ed842ae140a806c49d65506057a29bf22a6c
rebecca3miller <rebecca.miller@keyin.com>
2026-07-20 13:41:44 -0230

    Student.java file now extends to person.java. Created constructor and methods.

--- PROBLEM-1/Student.java
+++ PROBLEM-1/Student.java
@@ -1,5 +1,38 @@
-package PROBLEM-1;
-
-public class Student {
+public class Student extends Person {
     
+    protected String myIdNum;
+    protected double myGPA;
+
+    public Student(String name, int age, String gender, String idNum, double gpa) 
+    {
+        super(name, age, gender);
+
+        myIdNum = idNum;
+        myGPA = gpa;
+    }
+
+    public String getIdNum() 
+    {
+        return this.myIdNum;
+    }
+
+    public void setIdNum(String idNum) 
+    {
+        this.myIdNum = idNum;
+    }
+
+    public double getGPA() 
+    {
+        return this.myGPA;
+    }
+
+    public void setGPA(double gpa) 
+    {
+        this.myGPA = gpa;
+    }
+
+    public String toString() 
+    {
+        return super.toString() + ", Student ID: " + myIdNum + ", GPA: " + myGPA;
+    }
 }
